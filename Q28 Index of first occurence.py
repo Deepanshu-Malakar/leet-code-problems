50affd66bf9e24305fb3699d9e0b6bcf39bd46cb
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-05-21 15:34:01 +0530

    Create Q28 Index of first occurence.py

---
+++ Q28 Index of first occurence.py
@@ -0,0 +1,24 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            x=haystack.index(needle)
+            return x
+        else:
+            return -1
