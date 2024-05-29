bc3fb0d5111a36eb35783b7695d47720ee98e269
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-05-29 14:51:52 +0530

    Create Q68 sqrt of x.py

---
+++ Q68 sqrt of x.py
@@ -0,0 +1,27 @@
+"""
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        for i in range(0,x+1):
+            if i*i==x:
+                return i
+            elif i*i>=x:
+                return i-1
+        return 0
